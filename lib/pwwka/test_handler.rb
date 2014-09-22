ab5836757754b23ab5b1105d7783cd6439f0a99e
Dave Copeland <davec@naildrivin5.com>
2014-09-22 13:38:20 -0400

    fix small bug in testhandler and improve it
    * Docs about how to use it
    * Have it bind to *all* messages, not just `*.*`
    * Deprecate the `get_topic_blah_blah` for a more streamlined API

--- lib/pwwka/test_handler.rb
+++ lib/pwwka/test_handler.rb
@@ -1,4 +1,12 @@
 module Pwwka
+  # A handler you can use to examine messages your app sends during tests.
+  #
+  # To use this:
+  #
+  # 1. Create an instance and arrange for `test_setup` to be called when
+  #    your tests are being setup (e.g.`def setup` or `before`)
+  # 2. Arrange for `test_teardown` to be called during teardown of your tests
+  # 3. Use the method `pop_message` to examine the message on the queue
   class TestHandler
 
     attr_reader :channel_connector
@@ -21,19 +29,35 @@ module Pwwka
     def test_queue
       @test_queue  ||= begin
                          test_queue  = channel.queue("test-queue", durable: true)
-                         test_queue.bind(topic_exchange, routing_key: "*.*")
+                         test_queue.bind(topic_exchange, routing_key: "#.#")
                          test_queue
                        end
     end
 
+    # Get the message on the queue as TestHandler::Message.
+    def pop_message
+      delivery_info, properties, payload = test_queue.pop
+      Message.new(delivery_info,
+                  properties,
+                  payload)
+    end
+
     def get_topic_message_payload_for_tests
-      delivery_info, properties, payload  = test_queue.pop
-      JSON.parse(payload)
+      deprecated!(:get_topic_message_payload_for_tests,
+                  "Use `pop_message.payload` instead")
+      pop_message.payload
     end
 
     def get_topic_message_properties_for_tests
-      delivery_info, properties, payload  = test_queue.pop
-      properties
+      deprecated!(:get_topic_message_properties_for_tests,
+                  "Use `pop_message.properties` instead")
+      pop_message.properties
+    end
+
+    def get_topic_message_delivery_info_for_tests
+      deprecated!(:get_topic_message_delivery_info_for_tests,
+                  "Use `pop_message.delivery_info` instead")
+      pop_message.delivery_info
     end
 
     def purge_test_queue
@@ -44,6 +68,34 @@ module Pwwka
       test_queue.delete
       topic_exchange.delete
       channel_connector.connection_close 
+    end
+
+    # Simple class to hold a popped message.
+    #
+    # You can either access the message contents directly, or splat
+    # it for the most commonly-needed aspects:
+    #
+    #     delivery_info, payload = @test_handler.pop_message
+    class Message
+      attr_reader :delivery_info, :properties, :payload
+      def initialize(delivery_info, properties, payload)
+        @delivery_info = delivery_info
+        @properties = properties
+        @raw_payload = payload
+        @payload = JSON.parse(@raw_payload)
+      end
+
+      # Returns the delivery_info, payload, properties, and raw_payload for splat
+      # magic.
+      def to_ary
+        [@delivery_info,@payload,@properties,@raw_payload]
+      end
+    end
+
+  private
+
+    def deprecated!(method,message)
+      warn "#{method} is deprecated: #{message}"
     end
 
   end
