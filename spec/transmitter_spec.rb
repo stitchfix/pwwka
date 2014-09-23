c150bcffe7483004528ebdf9e8ac81fc003196b8
Nick Reavill <nick@fluxequalsrad.com>
2014-09-23 13:03:00 -0700

    try to get CI to pass

--- spec/transmitter_spec.rb
+++ spec/transmitter_spec.rb
@@ -46,7 +46,7 @@ describe Pwwka::Transmitter do
         success = Pwwka::Transmitter.new.send_delayed_message!(payload, routing_key, 1000)
         expect(success).to be_truthy
         expect(@test_handler.test_queue.message_count).to eq(0)
-        sleep 2
+        sleep 10
         expect(@test_handler.test_queue.message_count).to eq(1)
         received_payload = @test_handler.pop_message.payload
         expect(received_payload["this"]).to eq("that")
