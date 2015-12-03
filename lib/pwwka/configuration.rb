f331dfacca0dff43e6a980240fda8ebce6bf876e
Andrew Peterson <andy@ndpsoftware.com>
2015-12-03 15:03:39 -0800

    Add configuration for resque backoff strategy.

--- lib/pwwka/configuration.rb
+++ lib/pwwka/configuration.rb
@@ -9,6 +9,7 @@ module Pwwka
     attr_accessor :delayed_exchange_name
     attr_accessor :logger
     attr_accessor :options
+    attr_accessor :send_message_resque_backoff_strategy
 
     def initialize
       @rabbit_mq_host        = nil
@@ -16,6 +17,9 @@ module Pwwka
       @delayed_exchange_name = "pwwka.delayed.#{Pwwka.environment}"
       @logger                = MonoLogger.new(STDOUT)
       @options               = {}
+      @send_message_resque_backoff_strategy = [5,                  #intermittent glitch?
+                                               60,                 # quick interruption
+                                               600, 600, 600] # longer-term outage?
     end
 
 
