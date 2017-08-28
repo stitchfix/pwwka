a732b43574b34860d43c90a4e0f589507c07945e
David Copeland <davetron5000@gmail.com>
2017-08-28 14:48:50 -0400

    pass more info to Resque jobs, if configured

--- README.md
+++ README.md
@@ -301,7 +301,7 @@ If you use [Resque][resque], and you wish to handle messages in a resque job, yo
 2. Now, configure your handler.  For a `Procfile` setup:
 
    ```
-   my_handler: rake message_handler:receive HANDLER_KLASS=Pwwka::QueueResqueJobHandler JOB_KLASS=MyResqueJob QUEUE_NAME=my_queue ROUTING_KEY="my.key.completed"
+   my_handler: rake message_handler:receive HANDLER_KLASS=Pwwka::QueueResqueJobHandler JOB_KLASS=MyResqueJob PWWKA_QUEUE_EXTENDED_INFO=true QUEUE_NAME=my_queue ROUTING_KEY="my.key.completed"
    ```
 
    Note the use of the environment variable `JOB_KLASS`.  This tells `QueueResqueJobHandler` which class to queue.
@@ -311,16 +311,18 @@ If you use [Resque][resque], and you wish to handle messages in a resque job, yo
    class MyResqueJob
      @queue = :my_resque_queue
 
-     def self.perform(args) # i.e. payload
-       user = User.find(args.fetch("user_id")) # or whatever
+     def self.perform(payload,            # the payload
+                      routing_key,        # routing key as a string
+                      message_properties) # properties as a hash with _String_ keys
+       user = User.find(payload.fetch("user_id")) # or whatever
        user.frobnosticate!
      end
    end
    ```
 
-   Note that you must provide `@queue` in your job.  `QueueResqueJobHandler` doesn't support setting a custom queue and enqueue-time (PRs welcome :).
-   Note further that your job class is not given the routing key, so you'll have to set `ROUTING_KEY` appropriately for whatever it is you're trying to
-   do.
+   Note that you must provide `@queue` in your job.  `QueueResqueJobHandler` doesn't support setting a custom queue at enqueue-time (PRs welcome :).
+
+   Note that if you were using this library before version 0.11.0, your job would only be given the payload.  This is why `PWWKA_QUEUE_EXTENDED_INFO` must be set.  Without it, your job only gets the payload to avoid breaking legacy consumers.  You should always set this so you have access to the entire message.
 3. Profit!
 
 [resque]: https://github.com/resque/resque/tree/1-x-stable
--- spec/unit/queue_resque_job_handler_spec.rb
+++ spec/unit/queue_resque_job_handler_spec.rb
@@ -8,8 +8,16 @@ describe Pwwka::QueueResqueJobHandler do
 
   describe "::handle!" do
     let(:job_class) { MyTestJob }
-    let(:delivery_info) { double("delivery info") }
-    let(:properties) { double("properties") }
+    let(:routing_key) { "foo.bar.blah" }
+    let(:delivery_info) { double("delivery info", routing_key: routing_key) }
+    let(:properties_hash) {
+      {
+        "app_id" => "myapp",
+        "timestamp" => "2015-12-12 13:22:99",
+        "message_id" => "66",
+      }
+    }
+    let(:properties) { Bunny::MessageProperties.new(properties_hash) }
     let(:payload) {
       {
         "this" => "is",
@@ -21,16 +29,34 @@ describe Pwwka::QueueResqueJobHandler do
     before do
       allow(Resque).to receive(:enqueue)
       ENV["JOB_KLASS"] = MyTestJob.name
-
-      described_class.handle!(delivery_info,properties,payload)
     end
 
-    it "should queue a resque job using JOB_KLASS and payload" do
-      expect(Resque).to have_received(:enqueue).with(MyTestJob,payload)
+    context "when not asking for more information explicitly" do
+      it "should queue a resque job using JOB_KLASS and the payload" do
+        described_class.handle!(delivery_info,properties,payload)
+        expect(Resque).to have_received(:enqueue).with(MyTestJob,payload)
+      end
+    end
+
+    context "when asking to NOT receive more information explicitly" do
+      it "should queue a resque job using JOB_KLASS and the payload" do
+        ENV["PWWKA_QUEUE_EXTENDED_INFO"] = 'false'
+        described_class.handle!(delivery_info,properties,payload)
+        expect(Resque).to have_received(:enqueue).with(MyTestJob,payload)
+      end
+    end
+
+    context "when asking for more information via PWWKA_QUEUE_EXTENDED_INFO" do
+      it "should queue a resque job using JOB_KLASS, payload, routing key, and properties as a hash" do
+        ENV["PWWKA_QUEUE_EXTENDED_INFO"] = 'true'
+        described_class.handle!(delivery_info,properties,payload)
+        expect(Resque).to have_received(:enqueue).with(MyTestJob,payload,routing_key,properties_hash)
+      end
     end
 
     after do
       ENV.delete("JOB_KLASS")
+      ENV.delete("PWWKA_QUEUE_EXTENDED_INFO")
     end
   end
 end
