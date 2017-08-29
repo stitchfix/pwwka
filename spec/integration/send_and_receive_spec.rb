5d6b900fe20d9adb0e99ecf43728e9ece46a5ef2
David Copeland <davetron5000@gmail.com>
2017-08-29 08:10:13 -0400

    welp

--- spec/integration/send_and_receive_spec.rb
+++ spec/integration/send_and_receive_spec.rb
@@ -35,7 +35,7 @@ describe "sending and receiving messages", :integration do
   after do
     @testing_setup.kill_threads_and_clear_queues
     ENV.delete("JOB_KLASS")
-    ENV.delette("PWWKA_QUEUE_EXTENDED_INFO")
+    ENV.delete("PWWKA_QUEUE_EXTENDED_INFO")
   end
 
   context "routing" do
