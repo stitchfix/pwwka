38513713196cb60da8657ed81620348350a53086
David Copeland <davetron5000@gmail.com>
2017-10-21 12:43:20 -0400

    Ensure that payloads are logged in errors

--- lib/pwwka/error_handlers/chain.rb
+++ lib/pwwka/error_handlers/chain.rb
@@ -8,7 +8,7 @@ module Pwwka
         @error_handlers = default_handler_chain
       end
       def handle_error(message_handler_klass,receiver,queue_name,payload,delivery_info,exception)
-        logf "%{message_handler_klass} has received %{exception}", at: :error, message_handler_klass: message_handler_klass, exception: exception.message
+        logf "Error Processing Message in %{message_handler_klass} due to %{exception} from payload '%{payload}'", at: :error, message_handler_klass: message_handler_klass, exception: exception.message, payload: payload
         if message_handler_klass.respond_to?(:error_handler)
           @error_handlers.unshift(message_handler_klass.send(:error_handler))
         end
