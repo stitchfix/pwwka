9bb952299cb7778a9b7ef00ac0402adcdfd22485
Ross Hambrick <rosshambrick@gmail.com>
2019-01-16 12:39:10 -0500

    Logging errors that occur on a channel

--- lib/pwwka/channel_connector.rb
+++ lib/pwwka/channel_connector.rb
@@ -26,6 +26,9 @@ module Pwwka
 
       begin
         @channel = @connection.create_channel
+        @channel.on_error do |ch, method|
+          logf "ERROR On RabbitMQ channel: #{method.inspect}"
+        end
       rescue => e
         logf "ERROR Opening RabbitMQ channel", error: e
         @connection.close if @connection
