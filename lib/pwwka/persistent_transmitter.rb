432f10b509e91f284747c829d2c6812a4a0ebcb2
Lesly Brezilien <lesly.brezilien@stitchfix.com>
2019-04-23 17:17:36 -0500

    makes new private and still accesible to class methods

--- lib/pwwka/persistent_transmitter.rb
+++ lib/pwwka/persistent_transmitter.rb
@@ -24,15 +24,7 @@ module Pwwka
     DEFAULT_DELAY_BY_MS = 5000
 
     attr_reader :channel_connector
-    private_class_method :new
-
-    def self.batch(routing_key, type: nil, headers: nil, message_id: :auto_generate)
-      transmitter = self.new
-      yeild(transmitter)
-    ensure
-      transmitter.channel_connector.connection_close
-    end
-
+   
     def initialize
       @channel_connector = ChannelConnector.new(connection_name: "p: #{Pwwka.configuration.app_id} #{Pwwka.configuration.process_name}".strip)
     end
@@ -51,23 +43,16 @@ module Pwwka
       true
     end
 
+    class << self
+        private :new
+        
+        def batch(routing_key, type: nil, headers: nil, message_id: :auto_generate)
+          transmitter = new
+          yield(transmitter)
+        ensure
+          transmitter.channel_connector.connection_close
+        end
 
-    def send_delayed_message!(payload, routing_key, delay_by = DEFAULT_DELAY_BY_MS, type: nil, headers: nil, message_id: :auto_generate)
-      channel_connector.raise_if_delayed_not_allowed
-      publish_options = Pwwka::PublishOptions.new(
-        routing_key: routing_key,
-        message_id: message_id,
-        type: type,
-        headers: headers,
-        expiration: delay_by
-      )
-      logf "START Transmitting Delayed Message on id[%{id}] %{routing_key} -> %{payload}", id: publish_options.message_id, routing_key: routing_key, payload: payload
-      channel_connector.create_delayed_queue
-      channel_connector.delayed_exchange.publish(payload.to_json,publish_options.to_h)
-      # if it gets this far it has succeeded
-      logf "END Transmitting Delayed Message on id[%{id}] %{routing_key} -> %{payload}", id: publish_options.message_id, routing_key: routing_key, payload: payload
-      true
     end
-
   end
 end
